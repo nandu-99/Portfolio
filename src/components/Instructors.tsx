95b19b0101f4764959fc2c0bcd2b141e3799e327
Pottabathini Vivekananda <p.vivekananda@moveinsync.com>
2024-10-02 17:59:20 +0530

    added iconCloud

--- src/components/Instructors.tsx
+++ src/components/Instructors.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Spotlight } from "./ui/Spotlight";
 import { AnimatedTooltip } from "./ui/animated-tooltip";
-
+import { IconCloud } from "./IconCloud";
 
 const instructors = [
   {
@@ -41,7 +41,7 @@ const instructors = [
     designation:"",
     image: "/techstack/MongoDB.png",
   },
-  
+
   {
     id: 7,
     name: "Express.js",
@@ -60,14 +60,14 @@ const instructors = [
     designation:"",
     image: "/techstack/NodeJS.png",
   },
-  
-  
+
+
   {
     id: 10,
     name: "Angular",
     designation:"",
     image: "/techstack/angular.png",
-  }, 
+  },
   {
     id: 11,
     name: "Python",
@@ -91,31 +91,49 @@ const instructors = [
     name: "Tailwind CSS",
     designation:"",
     image: "/techstack/tailwind.jpg",
-  }, 
+  },
   {
     id: 15,
     name: "Git",
     designation:"",
     image: "/techstack/git.png",
-  }, 
+  },
 ];
 
 function Instructors() {
+  const iconSlugs = [
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "mysql",
+    "mongodb",
+    "express",
+    "react",
+    "nodejs",
+    "angular",
+    "python",
+    "java",
+    "bootstrap",
+    "tailwindcss",
+    "git",
+  ];
   return (
-      <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-        <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="cyan"
-        />
-        <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8 ">
-          My Skills
-        </h2>
-        <p className="text-base md:text-lg text-white text-center mb-4">
-          My skills include a deep dive into several remarkable technologies that I've worked with.
-        </p>
-        <div className="flex flex-row items-center justify-center mb-10 w-full">
+    <div className="h-auto md:h-[70rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+      <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="cyan" />
+      <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8 ">
+        My Skills
+      </h2>
+      <p className="text-base md:text-lg text-white text-center mb-2">
+        My skills include a deep dive into several remarkable technologies that
+        I've worked with.
+      </p>
+      <div className="flex flex-col gap-10">
+        <IconCloud iconSlugs={iconSlugs} />
+        <div className="flex flex-row items-center justify-center w-full">
           <AnimatedTooltip items={instructors} />
         </div>
+      </div>
     </div>
   );
 }
