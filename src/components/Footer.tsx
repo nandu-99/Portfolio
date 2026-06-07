f9c3c2e9616a7ec74640afc9b6d224265a188b39
Pottabathini Vivekananda <vivekananda.99666@gmail.com>
2026-06-07 18:00:00 +0530

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-black text-gray-400 p-12">
       <p className="text-center text-lg pt-8 font-semibold text-white">
-        ⚡Created By Vivekananda | © 2024 All rights reserved. ™ ⚡
+        ⚡Created By Vivekananda⚡
       </p>
     </footer>
   );
